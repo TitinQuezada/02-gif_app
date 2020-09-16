a66e46ba7ff2732cf69f815c1a53f5ddfe8390de
Joan Titin <joanmanueltitin@gmail.com>
2020-09-15 22:45:24 -0400

    add docs

---
+++ docs/precache-manifest.00e129849f6b9e2d6131a7f43202592f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23222e949fd4ff84a56831429b4d04f4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d9adef057e3f9b20f48b",
+    "url": "/static/css/main.3d955a8a.chunk.css"
+  },
+  {
+    "revision": "2376d00d9d0d296ad903",
+    "url": "/static/js/2.b0878678.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b0878678.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9adef057e3f9b20f48b",
+    "url": "/static/js/main.eb8c1fca.chunk.js"
+  },
+  {
+    "revision": "2c7e0a9c18916f684e55",
+    "url": "/static/js/runtime-main.3083b03f.js"
+  }
+]);
